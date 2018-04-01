d626657ef1a697bbbafe65bd0b6df341bb4e6b2d
rresto <torresborja@gmail.com>
2018-04-01 13:56:14 +0200

    Dia 4 completado

--- core/src/com/kilobolt/screens/GameScreen.java
+++ core/src/com/kilobolt/screens/GameScreen.java
@@ -3,10 +3,18 @@ package com.kilobolt.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.kilobolt.gameworld.GameRenderer;
+import com.kilobolt.gameworld.GameWorld;
 
 public class GameScreen implements Screen {
+
+    private GameWorld world;
+    private GameRenderer renderer;
+
     public GameScreen() {
         Gdx.app.log("GameScreen", "Attached");
+        world = new GameWorld();    //inicializo world
+        renderer = new GameRenderer(world);  //inicializo renderer
     }
     @Override
     public void show() {
@@ -15,10 +23,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
-        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
-        // Fills the screen with the selected color
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        world.update(delta);
+        renderer.render();
     }
 
     @Override
--- desktop/src/com/kilobolt/zombiebird/desktop/DesktopLauncher.java
+++ desktop/src/com/kilobolt/zombiebird/desktop/DesktopLauncher.java
@@ -5,14 +5,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.kilobolt.zombiebird.ZBGame;
 
 
-//TODO http://www.kilobolt.com/day-4-gameworld-and-gamerenderer-and-the-orthographic-camera.html
+//TODO http://www.kilobolt.com/day-5-the-flight-of-the-dead---adding-the-bird.html
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Zombie Bird";
-		config.width = 480;
-		config.height = 320;
+		config.width = 272;
+		config.height = 408;
 		new LwjglApplication(new ZBGame(), config);
 	}
 }
