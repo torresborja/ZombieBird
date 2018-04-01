da9f8774218a01b26558cf59636901e4d0106e31
rresto <torresborja@gmail.com>
2018-04-01 13:21:41 +0200

    Inicial

---
+++ desktop/src/com/kilobolt/zombiebird/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.kilobolt.zombiebird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kilobolt.zombiebird.ZBGame;
+
+
+//TODO http://www.kilobolt.com/day-4-gameworld-and-gamerenderer-and-the-orthographic-camera.html
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Zombie Bird";
+		config.width = 480;
+		config.height = 320;
+		new LwjglApplication(new ZBGame(), config);
+	}
+}
