ab5806925a65bcffc23f742892da0bbf74d6b483
rresto <torresborja@gmail.com>
2018-04-02 12:40:52 +0200

    Finalizado dia 7

--- core/src/com/kilobolt/gameworld/GameWorld.java
+++ core/src/com/kilobolt/gameworld/GameWorld.java
@@ -2,22 +2,31 @@ package com.kilobolt.gameworld;
 
 
 import com.kilobolt.gameobjects.Bird;
+import com.kilobolt.gameobjects.ScrollHandler;
 
 public class GameWorld {
 
     private Bird bird;
+    private ScrollHandler scroller;
 
     public GameWorld(int midPointY) {
         // Initialize bird here
         bird = new Bird(33, midPointY - 5, 17, 12);
+        // The grass should start 66 pixels below the midPointY
+        scroller = new ScrollHandler(midPointY + 66);
     }
 
     public void update(float delta) {
         bird.update(delta);
+        scroller.update(delta);
     }
 
     public Bird getBird() {
         return bird;
 
+    }
+
+    public ScrollHandler getScroller() {
+        return scroller;
     }
 }
--- desktop/src/com/kilobolt/zombiebird/desktop/DesktopLauncher.java
+++ desktop/src/com/kilobolt/zombiebird/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.kilobolt.zombiebird.ZBGame;
 
 
-//TODO http://www.kilobolt.com/day-7-the-grass-the-bird-and-the-skull-pipe.html
+//TODO http://www.kilobolt.com/day-8-collision-detection-and-sound-effects.html
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
